811d1a82c8d0e5dc93cb7bbabddc41e02c0f3030
tadashi.horii <sermentofsyana@gmail.com>
2020-01-27 23:32:13 +0000

    create index show new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,18 @@
 class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+    end
+    
+    def show
+        @task = Task.find(params[:id])
+    end
+    
+    def new
+        @task = Task.new
+    end
+    
 end
+
+
+#indexでnilclassになった要因→コントローラーにて、モデル名_controller.rbに記載しないといけないコードをapplication_controllerに書いていた %>
+#もう一つアクセスエラーになったのは、routesのとこでmessage#indexと記載していた。（本来tasks）%>
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    root to: 'message#index'
+    root to: 'tasks#index'
     
     resources :tasks
 end

1b06b2b517305ad57fb9780281952247f274d2eb
tadashi.horii <sermentofsyana@gmail.com>
2020-01-28 03:56:14 +0000

    add tasks#edit _flash_tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,8 +11,33 @@ class TasksController < ApplicationController
         @task = Task.new
     end
     
+    def create
+        @task = Task.new(task_params)
+        if @task.save
+            flash[:success] = 'タスクが登録されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'タスクが登録できませんでした'
+            render :new
+        end
+    end
+    
+    def edit
+        @task = Task.find(params[:id])
+    end
 end
 
+
+private
+
+#strong parameter
+def task_params
+    params.require(:task).permit(:content)
+end
+
+#task_params...は入力フォームに入れた内容をフィルターをかけて読み取る
+#フィルタリング処理後に残った文字列をTask.new(task_params)で引数として受け取り、それを@taskへ代入してる
+#require(:)の中身はモデル名（小文字）を入れる
 
 #indexでnilclassになった要因→コントローラーにて、モデル名_controller.rbに記載しないといけないコードをapplication_controllerに書いていた %>
 #もう一つアクセスエラーになったのは、routesのとこでmessage#indexと記載していた。（本来tasks）%>

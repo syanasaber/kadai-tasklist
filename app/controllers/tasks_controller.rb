41ff59542c1653b861c43e7823854804187538fb
tadashi.horii <sermentofsyana@gmail.com>
2020-01-28 10:00:40 +0000

    create complete tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,6 +25,25 @@ class TasksController < ApplicationController
     def edit
         @task = Task.find(params[:id])
     end
+    
+    def update
+        @task = Task.find(params[:id])
+        if @task.update(task_params)
+            flash[:success] = 'タスクが更新されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'タスクが更新されませんでした'
+            render :edit
+        end
+    end
+    
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        
+        flash[:success] = 'タスクは正常に削除されました'
+        redirect_to tasks_url
+    end
 end
 
 
@@ -35,9 +54,13 @@ def task_params
     params.require(:task).permit(:content)
 end
 
+
 #task_params...は入力フォームに入れた内容をフィルターをかけて読み取る
 #フィルタリング処理後に残った文字列をTask.new(task_params)で引数として受け取り、それを@taskへ代入してる
 #require(:)の中身はモデル名（小文字）を入れる
 
 #indexでnilclassになった要因→コントローラーにて、モデル名_controller.rbに記載しないといけないコードをapplication_controllerに書いていた %>
 #もう一つアクセスエラーになったのは、routesのとこでmessage#indexと記載していた。（本来tasks）%>
+
+#@task.update(task_params)の意味、指定のidで取り出したタスクに対して、新たに文字を入力して内容が、
+#privateクラスのtask_paramsメソッドでフィルタリングされて残った文字列が引数として@task.update(task_params)に放り込まれ、無事更新に成功すればflash[:success]となる

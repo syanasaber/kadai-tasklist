3d333c0271d85a218e0297517327329201032897
tadashi.horii <sermentofsyana@gmail.com>
2020-01-29 23:02:18 +0000

    add status, validate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_145548) do
+ActiveRecord::Schema.define(version: 2020_01_29_113511) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "status"
   end
 
 end
